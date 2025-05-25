cea30ba078fb9b7e71c070401ccb7ffa6c168883
Nico191196 <123272234+Nico191196@users.noreply.github.com>
2025-05-25 01:37:44 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,19 @@
 // js/firebase-config.js
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAvHK_BuP7HWDz_VAlCVylRKCZWI-qidCM",
+  authDomain: "ecoalertas-946d0.firebaseapp.com",
+  projectId: "ecoalertas-946d0",
+  storageBucket: "ecoalertas-946d0.firebasestorage.app",
+  messagingSenderId: "182951073757",
+  appId: "1:182951073757:web:8b7cf79eae56da91304834"
 };
-
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "…firebaseapp.com",
+  projectId: "...",
+  storageBucket: "…",
+  messagingSenderId: "...",
+  appId: "..."
+};
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
