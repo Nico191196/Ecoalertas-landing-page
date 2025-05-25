81d0f03a88a8334f9aea5eb125d8f0ab3a08fd4b
Nico191196 <123272234+Nico191196@users.noreply.github.com>
2025-05-24 21:50:03 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.querySelectorAll('.accordion-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const panel = btn.nextElementSibling;
-    panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + 'px';
-  });
-});
